d3de368a8fe79f3fe04dea709dc03353678b792c
alixixialy <148627205+alixixialy@users.noreply.github.com>
2023-12-02 11:02:13 +0800

    Update 删除.php

--- 干事请假系统/删除.php
+++ 干事请假系统/删除.php
@@ -1,11 +1,57 @@
 <?php
 include_once("connect.php");
-$sqlstr1 = "delete from tb_demo02 where id =".$_POST['id'];		//定义删除语句
-$result = mysqli_query($conn,$sqlstr1);				//执行删除操作
-if ($result){
-    echo "<script>alert('删除成功');location='Remove_Pages.html';</script>";  //删除成功则返回操作页面
-}else{
-    exit;
-}
-?>
+$con = $GLOBALS['con'];
+session_start();
 
+class timedeleted
+{
+    private $con;
+
+    public function __construct($con)
+    {
+        $this->con = $con;
+    }
+
+    public function deletedtime()
+    {
+        if (!isset($_SESSION['id'])) {
+            header('Location: index.html');
+        }
+
+        if (isset($_POST['Submit'])) {
+            $user_id = $_SESSION['id'];
+            $time = mysqli_real_escape_string($this->con, $_POST['time']);
+
+            $deletetimeSql = "SELECT * FROM tb_demo02 WHERE id = '$user_id' AND time = '$time'";
+            $deletetimeResult = mysqli_query($this->con, $deletetimeSql);
+
+            if (mysqli_num_rows($deletetimeResult) == 1) {
+                $updatePasswordSql = "DELETE FROM tb_demo02 WHERE id = 'id' AND time=$time";
+
+                if (mysqli_query($this->con, $deletetimeSql)) {
+                    $response = [
+                        'status' => 'success',
+                        'message' => 'time deleted successfully'
+                    ];
+                } else {
+                    $response = [
+                        'status' => 'error',
+                        'message' => 'Error deleting time: ' . mysqli_error($this->con)
+                    ];
+                }
+            } else {
+                $response = [
+                    'status' => 'error',
+                    'message' => 'Current time is incorrect'
+                ];
+            }
+
+            echo json_encode($response);
+            exit;
+        }
+    }
+}
+
+$timedeleted = new timedeleted($con);
+$timedeleted->deletedtime();
+?>
